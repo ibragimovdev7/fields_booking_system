654b1092a339c095a8ef34d4837790da6ea2e4ac
Ibragimov Abdug'ani <ibragimovgani0103@gmail.com>
2024-09-24 03:07:39 +0500

    last changes

--- football/filters.py
+++ football/filters.py
@@ -1,8 +1,10 @@
 import django_filters
-from django.db.models import Q, F
+from django.db.models import Q, F, OuterRef, Exists
 from django.db.models.functions import Sqrt, Power, Cos, Radians
 from django.utils.dateparse import parse_datetime
+from rest_framework.exceptions import ValidationError
 
+from bookings.models import Booking
 from football.models import FootballField
 
 
@@ -18,27 +20,41 @@ class FootbalFieldFilter(django_filters.FilterSet):
         fields = ['name', 'address', 'time']
 
     def time_filter(self, queryset, name, value):
-        start_datetime_str, end_datetime_str = value.split(',')
-        start_time = parse_datetime(start_datetime_str)
-        end_time = parse_datetime(end_datetime_str)
-        if start_time and end_time:
-            queryset = queryset.filter(
-                ~Q(bookings__start_time__lt=end_time) &
-                ~Q(bookings__end_time__gt=start_time)
-            )
+        if ',' not in value:
+            raise ValidationError(detail='"," bilan ajratilgan sana oraligi majburiy')
+        try:
+            start_datetime_str, end_datetime_str = value.split(',')
+            start_time = parse_datetime(start_datetime_str)
+            end_time = parse_datetime(end_datetime_str)
+        except Exception as e:
+            raise ValidationError(detail=f'{e}')
+
+        queryset = queryset.annotate(
+            is_booked=Exists(Booking.objects.filter(
+                field_id=OuterRef('pk'),
+                start_time__lte=start_time,
+                end_time__gte=end_time
+            ))
+        ).filter(is_booked=False)
         return queryset
 
     def distance_filter(self, queryset, name, value):
+        if ',' not in value:
+            raise ValidationError(detail='"," bilan ajratilgan kordinatalar majburiy')
+        if len(value.split(',')) == 3:
+            raise ValidationError(detail=f'3 ta qiymat kutilgan, ammo {len(value.split(","))} ta qiymat yuborilgan')
         user_lat, user_lon, radius = value.split(',')
-        if user_lat and user_lon and radius:
+        try:
             user_lat = float(user_lat)
             user_lon = float(user_lon)
             radius = float(radius)
+        except Exception as e:
+            raise ValidationError(detail=f'{e}')
 
-            queryset = queryset.annotate(
-                distance=Sqrt(
-                    Power(69.1 * (F('latitude') - user_lat), 2) +
-                    Power(69.1 * (user_lon - F('longitude')) * Cos(Radians(F('latitude'))), 2)
-                )
-            ).filter(distance__lte=radius).order_by('distance')
+        queryset = queryset.annotate(
+            distance=Sqrt(
+                Power(69.1 * (F('latitude') - user_lat), 2) +
+                Power(69.1 * (user_lon - F('longitude')) * Cos(Radians(F('latitude'))), 2)
+            )
+        ).filter(distance__lte=radius).order_by('distance')
         return queryset
--- football/serializers.py
+++ football/serializers.py
@@ -1,3 +1,7 @@
+from datetime import datetime, timedelta
+
+from django.utils.dateparse import parse_datetime
+from django.utils.timezone import make_aware
 from rest_framework import serializers
 from .models import FootballField, FieldImage
 
@@ -11,14 +15,36 @@ class FieldImageSerializer(serializers.ModelSerializer):
 class FootballFieldSerializer(serializers.ModelSerializer):
     images = FieldImageSerializer(many=True, read_only=True)
     distance = serializers.FloatField(allow_null=True, read_only=True)
-    broned_times = serializers.SerializerMethodField(method_name='get_broned_times')
+    free_times = serializers.SerializerMethodField(method_name='get_free_times')
 
     class Meta:
         model = FootballField
         fields = ['id', 'name', 'address', 'latitude',
                   'longitude', 'price_per_hour', 'contact',
-                  'images', 'distance', 'broned_times']
+                  'images', 'distance', 'free_times']
 
-    def get_broned_times(self, instance):
+    def get_free_times(self, instance):
         print('------', instance)
-        return 'x'
+        start_time = make_aware(datetime.now())
+        end_time = make_aware(datetime.now() + timedelta(days=7))
+        free_slots = []
+
+        booked_times = sorted(instance.booking_list, key=lambda x: x['start_time']) if instance.booking_list else []
+        current_time = start_time
+
+        for booking in booked_times:
+            t1 = parse_datetime(booking['start_time'])
+            t2 = parse_datetime(booking['end_time'])
+            if current_time < t1:
+                free_slots.append({
+                    'start_time': current_time,
+                    'end_time': t1
+                })
+            current_time = max(current_time, t2)
+
+        if current_time < end_time:
+            free_slots.append({
+                'start_time': current_time,
+                'end_time': end_time
+            })
+        return free_slots
